e6437444d388ddc672706fc76c8ae235b9c0bcbb
DamiAdeeso <damilolaadeeso@gmail.com>
2023-12-23 23:53:41 +0100

    worked on something

--- backend/src/main/java/com/kint/SnapCard/config/WebMvcConfiguration.java
+++ backend/src/main/java/com/kint/SnapCard/config/WebMvcConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableWebMvc
 public class WebMvcConfiguration {
 
 }
