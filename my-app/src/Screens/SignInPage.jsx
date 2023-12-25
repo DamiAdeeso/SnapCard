c62236d1708b83baeb9984945c6f49b1c28b4dfe
DamiAdeeso <damilolaadeeso@gmail.com>
2023-12-25 23:52:23 +0100

    finished signin functionality

--- my-app/src/Screens/SignInPage.jsx
+++ my-app/src/Screens/SignInPage.jsx
@@ -1,41 +1,50 @@
-import { Button, Col, Form ,Row} from 'react-bootstrap';
+import { Button, Col, Form, Row } from 'react-bootstrap';
 import "../styles/signin.css";
 import { useReducer, useState } from 'react';
 import LoadingBox from '../components/LoadingBox';
 import { FaArrowRight } from 'react-icons/fa';
 import axios from 'axios';
 import logo from "./animation.png"
+import { Toastify } from 'toastify';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
-const reducer = (state,action)=>{
-  switch(action.type){
+const reducer = (state, action) => {
+  switch (action.type) {
     case "SIGNIN_REQUEST":
-      return{...state,loading:true}
+      return { ...state, loading: true }
     case "SIGNIN_SUCCESS":
-      return {...state,loading:false}
+      return { ...state, loading: false }
     case "SIGNIN_ERROR":
-      return{...state,loading:false}
+      return { ...state, loading: false }
   }
 }
-  
+
 function SignInPage() {
 
-  const [email,setEmail]= useState("");
-  const [password,setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-
-  const [{loading},dispatch] = useReducer(reducer,{
-    loading:false
+  const [{ loading }, dispatch] = useReducer(reducer, {
+    loading: false
   })
-  
-  const signin = async (e)=>{
+
+  const signin = async (e) => {
     e.preventDefault();
-    try{
-      const {user} = await axios.put("/signin",{
+    try {
+      dispatch({ type: "SIGNIN_REQUEST" })
+      const userInfo = await axios.post("/api/v1/auth/signin", {
         email,
         password
-      })
-    }catch(err){
-      
+      });
+      localStorage.setItem("userInfo",userInfo)
+      toast.success("Signed In")
+      dispatch({ type: "SIGNIN_SUCCESS" });
+      navigate("/dashboard");
+    } catch (err) {
+      toast.error(err);
+      dispatch({ type: "SIGNIN_ERROR" });
     }
   }
 
@@ -43,15 +52,15 @@ function SignInPage() {
     <div id='signup-page'>
       <Row className='p-3' id='signup-row'>
         <h1 className='m-1'>Sign Up</h1>
-        <Col xs ={{span:12,order:2} } md={{span:5,order:1}} classname="m-4">
+        <Col xs={{ span: 12, order: 2 }} md={{ span: 5, order: 1 }} classname="m-4">
           <Form onSubmit={signin}>
-                  <Form.Control md ={6} type="email" className="m-2" placeholder="Email" onChange = {(e)=>{setEmail(e.target.value)}}/>
+            <Form.Control md={6} type="email" className="m-2" placeholder="Email" onChange={(e) => { setEmail(e.target.value) }} />
 
-                  <Form.Control  type="password" className="m-2" placeholder="Password" onChange={(e)=>{setPassword(e.target.value)}} />
-            {loading?<LoadingBox/>:<Button type="submit" id = "button" className='px-4 mt-3 mb-5'><FaArrowRight fill= "#ffffff"/></Button>}
+            <Form.Control type="password" className="m-2" placeholder="Password" onChange={(e) => { setPassword(e.target.value) }} />
+            {loading ? <LoadingBox /> : <Button type="submit" id="button" className='px-4 mt-3 mb-5'><FaArrowRight fill="#ffffff" /></Button>}
           </Form>
         </Col>
-        <Col xs={{span:12,order:1}} md={{span:7,order:2}} id='test' className='d-flex align-items-center justify-content-center'>
+        <Col xs={{ span: 12, order: 1 }} md={{ span: 7, order: 2 }} id='test' className='d-flex align-items-center justify-content-center'>
           <img src={logo} alt="logo" id="animation" />
         </Col>
       </Row>
